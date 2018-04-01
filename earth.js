fbf3725b4b2d20c92591f79db9b6e96d45a2fde1
Kai Garrott <garrottkai@gmail.com>
2018-03-31 19:48:42 -0600

    adding data points, initially just as cylinders. need to fix errors caused by Three version

--- earth.js
+++ earth.js
@@ -27,6 +27,44 @@ var geometry = new THREE.SphereGeometry(15, 48, 48);
 var mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh /*, lighting*/ );
 
+var getArray = (csv) => {
+  let lines = csv.split('\n');
+  lines.map(line => line.split(','));
+  return lines;
+};
+
+var addData = (data) => {
+  var dataGeometry = new THREE.Geometry();
+  var dataMaterial = new THREE.MeshBasicMaterial({
+    color: 0xFFFFFF
+  });
+  data.forEach(point => {
+    let [lat, long, val] = point;
+    let phi = (Math.PI / 180) * lat;
+    let theta = (Math.PI / 180) * (long - 180);
+
+    let x = -16 * Math.cos(phi) * Math.cos(theta);
+    let y = 16 * Math.sin(phi);
+    let z = 16 * Math.cos(phi) * Math.sin(theta);
+
+    let dataVector = new THREE.Vector3(x, y, z);
+    let dataCylinder = new THREE.Mesh(new THREE.CylinderBufferGeometry(.1, .1, 16, 5, 5));
+    //let dataCylinder = new THREE.CylinderBufferGeometry(.1, .1, 16, 5, 5);
+    dataCylinder.position = dataVector;
+    dataCylinder.lookAt(new THREE.Vector3(0, 0, 0));
+    dataGeometry.mergeMesh(dataCylinder);
+  });
+  let allData = new THREE.Mesh(dataGeometry, new THREE.MeshFaceMaterial());
+  scene.add(allData);
+};
+
+//$(document).ready(() => {
+jQuery.get('data/data.csv', data => {
+  data = getArray(data);
+  addData(data);console.log('wat')
+});
+//});
+
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
